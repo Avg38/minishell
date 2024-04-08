c3bdf60ce9100c10e370cf15c1eb934b25a3473d
justeozan <justeoz.an+projects@gmail.com>
2024-04-08 15:47:17 +0200

    delete headers

--- Amoza/libft/gc/gc_lstnew.c
+++ Amoza/libft/gc/gc_lstnew.c
@@ -1,16 +1,5 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   gc_lstnew.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: avialle- <avialle-@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/08 14:46:19 by avialle-          #+#    #+#             */
-/*   Updated: 2024/04/08 14:52:04 by avialle-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-valle-ville-850valle-valle-ville-4:8valle-valle-ville-84valle-valle-ville-836valle-valle-ville-832valle-valle-ville-825valle-valle-ville-89valle-valle-ville-809valle-valle-ville-800valle-51#include "../libft.h"
+#include "../libft.h"
 
 t_list	*gc_lstnew(void *content, int gc_id)
 {
